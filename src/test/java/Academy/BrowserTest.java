612aa98d5360d37766d43870ef1eed4443d1b934
Igor Babich <ibabich@aras.com>
2023-04-07 13:56:03 +0200

    Code review

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -9,27 +9,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-
 public class BrowserTest {
 
-	
 	@Test
 	public void getData()
 	{
 		System.out.println("Hello Guys Test");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		System.out.println("Test1");
 		WebDriver driver =new ChromeDriver();
-		System.out.println("Test2");
 		driver.get("https://testapp-dops.azurewebsites.net/webapp/");
-		System.out.println("Test3");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
-		System.out.println("Test");
 		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
 		driver.close();
-	
-		
-		
 	}
 }
